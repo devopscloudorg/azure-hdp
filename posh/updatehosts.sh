135b0e9cd8e57d607b9a9bc93e9eedc029475091
lararubbelke <larar@microsoft.com>
2014-03-19 09:02:32 -0500

    Update to updatehosts.sh in posh scripts

--- posh/updatehosts.sh
+++ posh/updatehosts.sh
@@ -2,22 +2,36 @@
 #This script reads the hosts file and merges its content with /etc/hosts
 
 #check to make sure hosts file exists
-echo "hosts file name is /root/hosts.txt"
+echo "hosts file name is:/root/hosts.txt"
+
+if [ -e /etc/hosts.bak ]
+then
+	rm /etc/hosts.bak
+fi
+
+#backup the hosts files
+cp /etc/hosts /etc/hosts.bak
+echo "########################################################before updating"
+cat /etc/hosts
 
 if [ -e /root/hosts.txt ]
 then
-	while read line;do
-        	echo "$line"
+	#read the hosts file for ip addresses. Look for that ip address in /etc/hosts file and remove it.
+	for line in `awk '{print $1}' < /root/hosts.txt`; do
+		echo $line
+		sed -i "/$line/d" /etc/hosts
+	done
 
-		if grep -Fxq "$line" /etc/hosts
-		then
-    			printf "$line already exists in hosts file\n"
-		else
-			echo $line >> /etc/hosts
-    			# code if not found
-		fi
-	done < /root/hosts.txt
+	cat /root/hosts.txt >> /etc/hosts
+
+	echo "######################################################################After updating"
+	cat /etc/hosts
 else
-	printf "File hosts.txt does not exist\n"
+	printf "File /root/hosts.txt does not exist\n"
 	exit 1
 fi
+
+echo "#############################################################################################"
+echo "Script finished successfully"
+echo "#############################################################################################"
+exit 0
